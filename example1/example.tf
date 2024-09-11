ff24aea5e3d551841d65b5ecdcbdf262ea9de259
Suryaraj Timsina <suryarajtimsina@Suryarajs-MacBook-Pro.local>
2024-09-11 20:02:34 +0545

    example 1 commit

---
+++ example1/example.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "cloud_suryaraj" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "instance_1" {
+  ami           = data.aws_ami.cloud_suryaraj.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Suryaraj Timsina"
+  }
+}
